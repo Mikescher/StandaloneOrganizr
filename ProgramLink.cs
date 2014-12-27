bd3b72c3fbb5acf8e63b39e79f91068fa90f2f2f
Mike Schwörer <mailport@mikescher.de>
2014-12-28 00:43:49 +0100

    Fixed Escaping

--- ProgramLink.cs
+++ ProgramLink.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace StandaloneOrganizr
@@ -32,13 +33,19 @@ namespace StandaloneOrganizr
 				throw new Exception("Invalid db file syntax");
 
 			name = head[0].Trim();
-			directory = Regex.Unescape(head[1].Trim().Trim('"'));
+			directory = Regex.Unescape(head[1].Trim());
+
+			if (!(directory.StartsWith("\"") && directory.EndsWith("\"")))
+				throw new Exception("Invalid db file syntax");
+
+			directory = directory.Substring(1, directory.Length - 2);
+
 			keywords = lines[1].Trim().Split(' ').Where(p => p.Trim() != "").ToList();
 		}
 
 		public string Save()
 		{
-			return name + ": \"" + Regex.Escape(directory) + "\"" + Environment.NewLine + "\t" + string.Join(" ", keywords);
+			return name + ": \"" + EscapeStr(directory) + "\"" + Environment.NewLine + "\t" + string.Join(" ", keywords);
 		}
 
 		public int Find(string search)
@@ -50,6 +57,72 @@ namespace StandaloneOrganizr
 				return 1;
 
 			return 0;
+		}
+
+		public static string EscapeStr(string value)
+		{
+			const char BACK_SLASH = '\\';
+			const char SLASH = '/';
+			const char DBL_QUOTE = '"';
+
+			var output = new StringBuilder(value.Length);
+			foreach (char c in value)
+			{
+				switch (c)
+				{
+					case SLASH:
+						output.AppendFormat("{0}{1}", BACK_SLASH, SLASH);
+						break;
+
+					case BACK_SLASH:
+						output.AppendFormat("{0}{0}", BACK_SLASH);
+						break;
+
+					case DBL_QUOTE:
+						output.AppendFormat("{0}{1}", BACK_SLASH, DBL_QUOTE);
+						break;
+
+					default:
+						output.Append(c);
+						break;
+				}
+			}
+
+			return output.ToString();
+		}
+
+		public static string UnescapeStr(string value)
+		{
+			const char BACK_SLASH = '\\';
+			const char SLASH = '/';
+			const char DBL_QUOTE = '"';
+
+			var output = new StringBuilder(value.Length);
+			bool esc = false;
+			foreach (char c in value)
+			{
+				if (esc)
+				{
+					output.Append(c);
+					esc = false;
+				}
+				else
+				{
+					switch (c)
+					{
+						case BACK_SLASH:
+							esc = true;
+							output.Append(c);
+							break;
+
+						default:
+							output.Append(c);
+							break;
+					}
+				}
+			}
+
+			return output.ToString();
 		}
 
 		public override string ToString()
